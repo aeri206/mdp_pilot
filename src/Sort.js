58980fdcc6752fe7004f43bcb05a954d0b897807
aeri206 <aeri33590@gmail.com>
2022-05-25 02:42:07 +0900

    description update

--- src/Sort.js
+++ src/Sort.js
@@ -1,7 +1,7 @@
 // // @ts-nocheck
 import React, {useState } from "react";
 
-import { Grommet, Table, TableHeader, TableRow, TableBody, TableCell, Select, DataTable, Box, Image, Page, PageContent, Button, Paragraph, Grid } from 'grommet';
+import { Grommet, Grid, Table, TableHeader, TableRow, TableBody, TableCell, Select, List, DataTable, Box, Image, Page, PageContent, Button, Paragraph } from 'grommet';
 
 
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
@@ -16,16 +16,12 @@ const dataNames = [
     'boston',
     'breastcancercoimbra',
     'breastcancerwisconsinprognostic',
-    'covertype',
-    'echocardiogram',
     'ecoli',
     'extyaleb',
     'glassidentification',
     'heartdisease',
     'hepatitis',
-    'housing',
     'iris',
-    'mnist64',
     'olive',
     'weather',
     'wine',
@@ -54,12 +50,34 @@ const selected =
     'wine': ['11', '29', '39', '62', '9'],
     'world12d': ['2', '45', '49', '58', '66']
 }
+
+const info = {
+  "boston":{"row":155,"class":3,"dim":13.0},
+  "breastcancercoimbra":{"row":116,"class":2,"dim":9.0},
+  "breastcancerwisconsinprognostic":{"row":569,"class":2,"dim":30.0},
+  "covertype":{"row":581012,"class":7,"dim":54.0},
+  "echocardiogram":{"row":61,"class":2,"dim":10.0},
+  "ecoli":{"row":336,"class":8,"dim":7.0},
+  "extyaleb":{"row":320,"class":5,"dim":30.0},
+  "glassidentification":{"row":214,"class":6,"dim":9.0},
+  "heartdisease":{"row":297,"class":5,"dim":13.0},
+  "hepatitis":{"row":80,"class":2,"dim":19.0},
+  "housing":{"row":688,"class":1,"dim":null},
+  "iris":{"row":150,"class":3,"dim":4.0},
+  "mnist64":{"row":1083,"class":6,"dim":64.0},
+  "olive":{"row":572,"class":3,"dim":8.0},
+  "weather":{"row":366,"class":7,"dim":192.0},
+  "wine":{"row":178,"class":3,"dim":13.0},
+  "world12d":{"row":151,"class":5,"dim":12.0}
+}
   
 
 
 const Sort = () => {
     
     const [dataName, setDataName] = useState(dataNames[0]);
+
+    const datasetInfo = info[dataName]
     
     const x = 'metric'
     const metricData = require(`/public/data/${x}_some.json`);
@@ -90,6 +108,18 @@ const Sort = () => {
             }}
           />
           </Box>
+          <Grid
+      alignSelf='center'
+  columns={[['large', 'large'], ['auto', 'large']]}
+  rows={['auto', 'flex']}
+  gap="small"
+  areas={[
+    { name: 'main', start: [0, 0], end: [0, 1] },
+    { name: 'instruction', start: [1, 0], end: [1, 0] },
+    { name: 'metric-info', start: [1, 1], end: [1, 1] },
+  ]}
+>
+  <Box gridArea="main">
       <DragDropContext onDragEnd={handleDragEnd}>
         <Table className="table borderd">
           <TableHeader border="bottom">
@@ -141,6 +171,53 @@ const Sort = () => {
           </Droppable>
         </Table>
       </DragDropContext>
+    </Box>
+    <Box gridArea="instruction" 
+      height={{ min: "small", max: "large" }}
+      background="light-2" style={{margin: "5px 0 10px 0", padding: "20px 0 10px 10px"}}>
+  당신은 데이터를 차원 축소 기법을 이용하여 분석하려고 합니다.
+  데이터의 기본적인 정보는 아래와 같습니다.
+
+  <Paragraph margin='5px 0 0 5px'>
+      {datasetInfo['dim']} dimension
+      </Paragraph>
+      <Paragraph margin='5px 0 0 5px'>
+    {datasetInfo['row']} row (data point)
+    </Paragraph>
+    
+    <Paragraph margin='5px 0 20px 5px'>
+    {datasetInfo['class']} class (label)
+  </Paragraph>
+
+  왼쪽에는 이 데이터에 대한 5개의 projection과 각각의 projection metric이 제시되어 있습니다. 
+    
+  <p/>
+  데이터 분석에 projection 전체를 사용하거나, 유용한 결론을 도출할 수 있는 projection을 선택적으로 사용할 수 있습니다. 
+  
+  <b>이 projection을 보고 데이터 분석에 가장 유용할 것 같다고 판단되는 순서대로 각 열(Row)을 나열하세요.</b>
+  <p/>
+  가장 유용하다고 판단한 기준과 가장 유용하지 않다고 판단한 기준은 무엇입니까? 
+
+  
+  </Box>
+  <Box gridArea="metric-info"
+    background="light-2" 
+  >
+    <List
+    border="horizontal"
+  primaryKey="name"
+  secondaryKey={(item) => <Paragraph>{item.description}</Paragraph>}
+  data={[
+    { name: 'Trustworthiness', description: '고차원에서의 local한 이웃구조가 저차원에서 잘 보존 (높을수록 좋다, 잘 보존한다)' },
+    { name: 'Cohesiveness', description: '고차원과 저차원간의 inter-cluster reliability, projection의 cluster가 고차원에서 얼마나 잘 보존하였는가 (높을수록 좋다, 잘 보존한다)' },
+    { name: 'DTM_KL01', description: 'HD - LD사이의 분포의 차이, global structure를 얼마나 잘 보존하였는가 (낮을수록 좋다)' },
+  ]}
+/>
+    
+  </Box>
+
+      
+    </Grid>
     </Grommet>
   );
 }
